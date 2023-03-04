416a732ad9655f6b78553bd1c56eba8cac7d136c
Ehren Dames <ehren@ehrendames.com>
2023-03-04 10:28:49 -0700

    fix: RichText component

--- nextjs/src/components/RichText.tsx
+++ nextjs/src/components/RichText.tsx
@@ -1,13 +1,13 @@
 import {
   RichText as SitecoreRichText,
-  Field,
   withDatasourceCheck,
+  RichTextField,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 type RichTextProps = ComponentProps & {
   fields: {
-    Text: Field<string>;
+    Text: RichTextField;
   };
 };
 
@@ -16,10 +16,12 @@ type RichTextProps = ComponentProps & {
  * This is the most basic building block of a content site, and the most basic
  * JSS component that's useful.
  */
-const RichText = ({ fields, rendering, params }: RichTextProps): JSX.Element => (
-  <div className="richText">
-    <SitecoreRichText fields={fields} rendering={rendering} params={params} />
-  </div>
-);
+const RichText: React.FC<RichTextProps> = (props) => {
+  return (
+    <div className="richText">
+      <SitecoreRichText field={props.fields.Text} />
+    </div>
+  );
+};
 
 export default withDatasourceCheck()<RichTextProps>(RichText);
